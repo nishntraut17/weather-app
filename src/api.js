bfa8203981fd86ade5fbea6dd29c0c73a2ac307a
Nishant Raut <89209436+nishntraut@users.noreply.github.com>
2023-02-13 10:10:21 +0530

    weather app

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c960ec27b8msh5f0df38a9e567c7p1f3a73jsn9ac4f48f727c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "7aa7440c4e28540bf73555de14f12da9";
